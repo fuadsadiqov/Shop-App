2508bf5f2e0224cc0618807f8a2cbb6bc6583981
fuadsadiqov <sadiqovfuad8@gmail.com>
2023-05-14 12:32:08 +0400

    first commit

--- data.js
+++ data.js
@@ -1,16 +1,16 @@
 module.exports = function(){
     return{
         products: [
-            {id: 1, name: "Iphone 12", price: 2000, imageUrl: "https://www.soliton.az/images/articles/2020/12/07/20201207035929034_c1_1.jpg", description: "Iphone 12 description", category: "Telefon"},
-            {id: 2, name: "Iphone 13", price: 2500, imageUrl: "https://irshad.az/resized/fit540x550/center/products/44260/08988b79170b2a78284f4be9f2a6b987.jpg", description: "Iphone 13 description", category: "Telefon"},
-            {id: 3, name: "Iphone 13 Pro", price: 3000, imageUrl: "https://maxi.az/upload/iblock/270/ymm4ufrxwq7p8rygem1uk5tplg9qcegm/telefon_iphone_13_pro_128gb_graphite_detail.jpg", description: "Iphone 13 Pro description", category: "Computer"},
-            {id: 4, name: "Iphone 14", price: 3500, imageUrl: "https://www.bakuelectronics.az/assets/images/products/118568/smartfon-apple-iphone-14-progold-1.jpg", description: "Iphone 14 description", category: "Telefon"},
-            {id: 5, name: "Iphone 14 Pro Max", price: 4000, imageUrl: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-14-pro-finish-select-202209-6-7inch-deeppurple?wid=5120&hei=2880&fmt=p-jpg&qlt=80&.v=1663703841896", description: "Iphone 14 Pro Max description", category: "Computer"},
+            {name: "Iphone 12", price: 2000, imageUrl: "https://www.soliton.az/images/articles/2020/12/07/20201207035929034_c1_1.jpg", description: "Iphone 12 description", category: "Telefon"},
+            {name: "Iphone 13", price: 2500, imageUrl: "https://irshad.az/resized/fit540x550/center/products/44260/08988b79170b2a78284f4be9f2a6b987.jpg", description: "Iphone 13 description", category: "Telefon"},
+            {name: "Iphone 13 Pro", price: 3000, imageUrl: "https://maxi.az/upload/iblock/270/ymm4ufrxwq7p8rygem1uk5tplg9qcegm/telefon_iphone_13_pro_128gb_graphite_detail.jpg", description: "Iphone 13 Pro description", category: "Computer"},
+            {name: "Iphone 14", price: 3500, imageUrl: "https://www.bakuelectronics.az/assets/images/products/118568/smartfon-apple-iphone-14-progold-1.jpg", description: "Iphone 14 description", category: "Telefon"},
+            {name: "Iphone 14 Pro Max", price: 4000, imageUrl: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-14-pro-finish-select-202209-6-7inch-deeppurple?wid=5120&hei=2880&fmt=p-jpg&qlt=80&.v=1663703841896", description: "Iphone 14 Pro Max description", category: "Computer"},
         ],
         categories: [
-            {id: 1, name: "Telefon"},
-            {id: 2, name: "Computer"},
-            {id: 3, name: "Technology device"}
+            {name: "Telefon"},
+            {name: "Computer"},
+            {name: "Technology device"}
         ],
         orders:  [
 
