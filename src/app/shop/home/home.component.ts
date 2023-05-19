1a3ff1c1ae514e653af5d199d77063e10176b431
fuadsadiqov <sadiqovfuad8@gmail.com>
2023-05-19 10:42:48 +0400

    adding home page

--- src/app/shop/home/home.component.ts
+++ src/app/shop/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Product } from 'src/app/model/product.model';
+import { ProductRepository } from 'src/app/model/product.repository';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  public selectedCategory: any
+  public productsPerPage = 3
+  public selectedPage = 1
+
+  constructor(
+    private productRepository: ProductRepository,
+  ){} 
+  get products(): Product[]{
+      let index = (this.selectedPage - 1) * this.productsPerPage
+      
+      return this.productRepository
+              .getProducts(this.selectedCategory)
+              .slice(index, index + this.productsPerPage)
+  }
 }
